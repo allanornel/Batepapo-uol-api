b43adcad48505bc7e0fa6edefd1dccb4d47c696d
allanornel <allanornel@gmail.com>
2022-04-28 02:43:16 -0300

    feat: participants done

--- index.js
+++ index.js
@@ -30,9 +30,16 @@ app.post("/participants", async (req, res) => {
   try {
     await mongoClient.connect();
     const db = mongoClient.db("projeto12");
-    await db.collection("participants").insertOne(objParticipante);
-    await db.collection("messages").insertOne(objMessage);
-    res.sendStatus(201);
+    const find = await db.collection("participants").findOne({ name });
+    console.log(find);
+    if (!find) {
+      await db.collection("participants").insertOne(objParticipante);
+      await db.collection("messages").insertOne(objMessage);
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(409);
+    }
+
     mongoClient.close();
   } catch (e) {
     console.log("Erro na tentativa de conectar ao banco de dados", e);
@@ -53,10 +60,68 @@ app.get("/participants", async (req, res) => {
   }
 });
 
-app.post("/messages", async (req, res) => {});
+app.post("/messages", async (req, res) => {
+  const from = req.headers.user;
+  const { to, text, type } = req.body;
+  // **to** e **text** devem ser strings não vazias
+  // **type** só pode ser 'message' ou 'private_message'
+  // **from** deve ser um participante existente na lista de participantes
 
-app.get("/messages", async (req, res) => {});
+  const obj = { from, to, text, type, time: dayjs().format("HH:mm:ss") };
+  console.log(obj);
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("projeto12");
+    await db.collection("messages").insertOne(obj);
+    res.status(201);
+    mongoClient.close();
+  } catch (e) {
+    console.log("Erro na tentativa de conectar ao banco de dados", e);
+    mongoClient.close();
+  }
+  res.sendStatus(201);
+});
 
-app.post("/status", async (req, res) => {});
+app.get("/messages", async (req, res) => {
+  const { user } = req.headers;
+  const limit = parseInt(req.query.limit);
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("projeto12");
+    const messages = await db.collection("messages").find({}).toArray();
+    if (limit) {
+      res.status(200).send(messages);
+    } else {
+      res.status(200).send(messages);
+    }
+    mongoClient.close();
+  } catch (e) {
+    console.log("Erro na tentativa de conectar ao banco de dados", e);
+    mongoClient.close();
+  }
+});
+
+app.post("/status", async (req, res) => {
+  const { user } = req.headers;
+
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("projeto12");
+    const find = await db
+      .collection("participants")
+      .find({ name: user })
+      .toArray();
+    if (find) {
+      // await db.collection("participants").findOneAndUpdate({ lastStatus: Date.now() });
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+    mongoClient.close();
+  } catch (e) {
+    console.log("Erro na tentativa de conectar ao banco de dados", e);
+    mongoClient.close();
+  }
+});
 
 app.listen(5000);
